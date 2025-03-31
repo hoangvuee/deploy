a1762e83f66ef478c361eae326121e61bd9985a1
NGUYEN BUI HOANG VU <22130328@st.hcmuaf.edu.vn>
2025-04-01 00:29:02 +0700

    thêm hàm đánh giá

--- src/main/java/Services/ServiceFeedback.java
+++ src/main/java/Services/ServiceFeedback.java
@@ -3,6 +3,7 @@ package Services;
 import Dao.FeedbackDao;
 import Models.Feedback.Feedback;
 
+import java.sql.Date;
 import java.util.List;
 
 public class ServiceFeedback {
@@ -10,4 +11,11 @@ public class ServiceFeedback {
     public List<Feedback> getFeedbacksByProductId(int productId) {
     return feedbackDao.getFeedbacksByProductId(productId);
     }
+    public int getIdOrderDetails(String idOrder, String idProduct){
+        return feedbackDao.getIdOrderDetails(idOrder,idProduct);
+    }
+    public void insertReview(String comment, int idOrderDetail, int ratingRank, String status){
+        feedbackDao.insertReview( comment,   idOrderDetail, ratingRank, status);
+    }
+
 }
