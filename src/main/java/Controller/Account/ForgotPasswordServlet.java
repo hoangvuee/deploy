c213bb348054dd1bbe3d26a52b729624eb3443f2
NGUYEN BUI HOANG VU <22130328@st.hcmuaf.edu.vn>
2025-04-04 04:01:31 +0700

    Gửi token cho người dùng để đổi mk

--- src/main/java/Controller/Account/ForgotPasswordServlet.java
+++ src/main/java/Controller/Account/ForgotPasswordServlet.java
@@ -3,6 +3,7 @@ package Controller.Account;
 import Models.User.User;
 
 
+import Services.ServiceResetToken;
 import Services.ServiceUser;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -16,6 +17,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import java.security.SecureRandom;
@@ -27,6 +29,7 @@ import java.util.Base64;
 )
 public class ForgotPasswordServlet extends HttpServlet  {
     ServiceUser serviceUser = new ServiceUser();
+    ServiceResetToken serviceResetToken = new ServiceResetToken();
 
 
     @Override
@@ -37,9 +40,38 @@ public class ForgotPasswordServlet extends HttpServlet  {
         String hashedPassword = null;
         String newPassword = null;
         if(user != null){
-             newPassword = serviceUser.generateRandomPassword();
+            String token = serviceResetToken.generateRandomToken();
+            try {
+                boolean isTokenSaved = serviceResetToken.saveResetToken(user.getId(), token);
+                if(isTokenSaved){
+                    // Tạo link chứa token
+                    String resetPasswordLink = "http://localhost:8080/WebFinall/Account/resetPassword.jsp?token=" + token;
+
+                    // Soạn thông điệp email
+                    String subject = "Thông Báo Đổi Mật Khẩu Tài Khoản Của Bạn";
+                    String messageContent = "Kính gửi " + user.getUserName() + ",\n" +
+                            "Để thay đổi mật khẩu tài khoản của bạn, vui lòng nhấn vào liên kết dưới đây:\n" +
+                            resetPasswordLink + "\n" +
+                            "Nếu bạn không yêu cầu thay đổi mật khẩu này, vui lòng bỏ qua email này.\n" +
+                            "Trân trọng,\n" +
+                            "[Đội ngũ hỗ trợ]";
+
+                    try {
+                        // Gửi email
+                        serviceUser.sendEmail(email, subject, messageContent);
+                        resp.sendRedirect("Account/login.jsp"); // Chuyển hướng đến trang đăng nhập
+                    } catch (MessagingException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
 
             try {
+
                 hashedPassword = serviceUser.hashPassword(newPassword);
             } catch (NoSuchAlgorithmException e) {
                 throw new RuntimeException(e);
